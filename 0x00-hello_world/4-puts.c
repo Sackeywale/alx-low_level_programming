8e255a532841d144e1ab70693930dc0a899e85a1
sackeywale <sackey619wale@gmail.com>
2022-10-14 18:50:37 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,12 @@
 #include <stdio.h>
-#include <string.h>
 
 /**
  *main function for string
  *
  *return 0 for everytime
  */
-
 int main()
 {
 puts("\Programming is like building a multilingual puzzle");
-
 return 0;
 }
