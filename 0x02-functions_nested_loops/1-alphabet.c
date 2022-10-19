9f29fb909b2b1bdf5c18e94b208ec77ddf2aef89
sackeywale <sackey619wale@gmail.com>
2022-10-18 17:41:57 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *main - print alphabets
+ *
+ *return - 0 if success
+ */
+
+void print_alphabet(void)
+{
+int a;
+
+for (a = 'a'; a <= 'z'; a++)
+{
+_putchar(a);
+}
+putchar('\n');
+
+return (0);
+}
