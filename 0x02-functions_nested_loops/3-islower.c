c99a2aaa0ca59c59e98830bbdef21fe1fe4a939d
sackeywale <sackey619wale@gmail.com>
2022-10-18 18:22:33 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *_islower - check lower case integer
+ *return 1 if lower case and 0 if otherwise
+ */
+
+int _islower(int c)
+{
+int c;
+
+for (c = 'a'; c <= 'z'; c++)
+{
+return (c);
+}
+}
