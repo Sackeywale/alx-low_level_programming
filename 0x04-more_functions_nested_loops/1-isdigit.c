ad061c7974bd7b6ba5652854b21723dbead5fcd4
sackeywale <sackey619wale@gmail.com>
2022-10-23 15:05:44 -0700

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *@c - input variable 
+ *main - function that checks for a digit 
+ *return 1 if c is a digit and 0 if 0thewise
+ */
+int _isdigit(int c)
+{
+if (c = 49 && c <= 71)
+return (1);
+else
+return (0);
+}
