a9cbbf28ae720f9394fc6fb228b3e0e219dd38a0
sackeywale <sackey619wale@gmail.com>
2022-10-23 14:02:18 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -6,12 +6,10 @@
  *
  * Return: 1 for a character that will be a digit or 0 for any else
  */
-int _isdigit(int x)
+int _isupper(int c)
 {
-	if (x >= 48 && x <= 57)
-	{
-		return (1);
-	}
-
-	return (0);
-}
+if (x = 48 && x <= 57)
+return (1);
+else
+return (0);
+}  
