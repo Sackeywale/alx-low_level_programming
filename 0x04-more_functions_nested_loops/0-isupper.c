cb3bffcf7d81041c11a4270b03f2ddb3244c113c
sackeywale <sackey619wale@gmail.com>
2022-10-23 14:12:58 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,14 +2,14 @@
 
 /**
  * _isdigit - Check if a character is a digit
- * @x: The number to be checked
- *
+ * @c: The number to be checked
+ * Main - check for alphabets
  * Return: 1 for a character that will be a digit or 0 for any else
  */
 int _isupper(int c)
 {
-if (x = 48 && x <= 57)
+if (c >= 65 && c <= 90)
 return (1);
 else
 return (0);
-}  
+}
