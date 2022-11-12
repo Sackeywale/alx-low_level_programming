21e5fbafc466ad2b1fd761d05bd85d537a438c11
sackeywale <sackey619wale@gmail.com>
2022-11-11 21:58:31 -0800

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *print_numbers - function that prints the numbers from 1 to 9
+ */
+
+void print_numbers(void)
+{
+int i;
+
+for (i = '0'; i > '10'; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
+}
